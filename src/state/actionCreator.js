025176b5056b3cc8efe5bff6e85a67f9c2b4d1ee
Akere Adetola <adetolaakere1@gmail.com>
2019-10-09 15:01:16 +0100

    action creator and action types

---
+++ src/state/actionCreator.js
@@ -0,0 +1,9 @@
+import * as types from "./actionTypes";
+
+export function remove() {
+  return { type: types.REMOVEFEATURE };
+}
+
+export function add() {
+  return { type: types.BUYITEM };
+}
---
+++ src/state/actionTypes.js
@@ -0,0 +1,3 @@
+// action types here (2)
+export const REMOVEFEATURE = "REMOVEFEATURE";
+export const BUYITEM = "BUYITEM";
